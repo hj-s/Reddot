480e1831ace41760d68bf30f2153433dba4388d5
hj-s <spam@hj-s.ru>
2018-07-19 12:11:58 +0500

    add stuff

--- js/reddot.js
+++ js/reddot.js
@@ -1,21 +1,22 @@
+//arrow fucntions
+const isDefined = (check) => (check !== undefined);
+
 //const or start data
 var cwidth = 640
 var cheight = 480
-var fwidth = 40
-var fheight = 40
 var sqfd = 20
-
-
 
 const rPlace = 0.3
 const dPlace = 0.7
+const extPlace = 0.5
 
 var maze = undefined
-var canvas = undefined
-var ctx = undefined
+var reddot = undefined
+var exit = undefined
+
 //int
 function init(){
-	canvas = document.getElementById('field')
+	var canvas = document.getElementById('field')
 	if (canvas.getContext) {
 		
 		//set canvase size
@@ -23,61 +24,261 @@ function init(){
 		canvas.width = cwidth
 
 		//get context
-		ctx = canvas.getContext('2d')
+		let ctx = canvas.getContext('2d')
+		ctx.save()
 		
-		//init field
-		maze = new SqfField(cwidth/sqfd, cheight/sqfd)
-		maze.createMaze('eller')
+		//add keypress listener
+		let d = document
+		d.addEventListener('keypress', handleKeys)
 
-		drawMaze(ctx, maze)
+		//startMaze
+		startMaze()
 
 	} else {
 		alert("Your browser does not support canvas")
 	}
 }
 
-//functions to draw stuff
-function drawMaze(ctx, maze){
-	var startX = 0;
-	var startY = 0;
-	for (let i = 0; i < maze.height; i++){
-		for (let j = 0; j < maze.width; j++){
-			if (maze.field[i][j].up){
-				drawLine(ctx, startX, startY, startX + sqfd, startY)
-			}
-			if (maze.field[i][j].right){
-				drawLine(ctx, startX + sqfd, startY, startX + sqfd, startY + sqfd)
-			}
-			if (maze.field[i][j].down){
-				drawLine(ctx, startX, startY + sqfd, startX + sqfd, startY + sqfd)
-			}
-			if (maze.field[i][j].left){
-				drawLine(ctx, startX, startY, startX, startY + sqfd)
-			}
-			startX += sqfd
+//create maze & reddot
+function startMaze(){
+	//clear all before start new
+	clearCanvas()
+
+	//create new fiels
+	maze = new SqfField(cwidth/sqfd, cheight/sqfd)
+	//create point
+	reddot = new Point(0,0)
+	//draw field of view
+	drawGradient()
+	//create maze using method
+	maze.createMaze('eller')
+	if (isDefined(maze)){
+		//draw maze
+		drawMaze()
+	}
+	//draw reddot
+	if (isDefined(reddot)){
+		drawReddot()
+	}
+
+	exit = createExit()
+	if (isDefined(exit)){
+		drawExit()
+	}
+}
+
+//create exit
+function createExit() {
+	if (isDefined(maze)){
+		//set wall
+		let walls = [`up`,`right`,`down`,`left`]
+		let randomItem = walls[Math.floor(Math.random()*walls.length)];
+		console.log(randomItem)
+		if (randomItem == `up`){
+			return new Point(Math.floor(Math.random()*cwidth/sqfd), 0)
+		}else if (randomItem == `right`) {
+			return new Point(cwidth/sqfd - 1, Math.floor(Math.random()*cheight/sqfd))
+		}else if (randomItem == `down`) {
+			return new Point(Math.floor(Math.random()*cwidth/sqfd), cheight/sqfd - 1)
+		}else if (randomItem ==`left` ) {
+			return new Point(0 ,Math.floor(Math.random()*cheight/sqfd))
+		}else {
+			return undefined
 		}
-		startX = 0
-		startY += sqfd
 	}
 }
-function drawLine(ctx, startX, startY, endX, endY){
-	ctx.moveTo(startX, startY)
-	ctx.lineTo(endX, endY)
-	ctx.stroke()
+//handle keys
+function handleKeys(event){
+	// if (event.defaultPrevented) {
+	// 	return
+	// }
+	if (isDefined(reddot)) {
+		let move = false
+		let key = event.key || event.keyCode
+		if (key == `W` || key == `w`) {
+			//check if can
+			if (!checkWall(reddot.x, reddot.y, reddot.x, reddot.y-1)){
+				reddot.y -= 1
+				move = true
+			}
+		}
+		if (key == `S` || key == `s`) {
+			//check if can
+			if (!checkWall(reddot.x, reddot.y, reddot.x, reddot.y+1)){
+				reddot.y += 1
+				move = true
+			}
+		}
+		if (key == `A` || key == `a`) {
+			//check if can
+			if (!checkWall(reddot.x, reddot.y, reddot.x-1, reddot.y)){
+				reddot.x -= 1
+				move = true
+			}
+		}
+		if (key == `D` || key == `d`) {
+			//check if can
+			if (!checkWall(reddot.x, reddot.y, reddot.x+1, reddot.y)){
+				reddot.x += 1
+				move = true
+			}
+		}
+		if (move){
+			//clear canvas
+			clearCanvas()
+			//draw field of view
+			drawGradient()
+			//redraw maze
+			drawMaze()
+			//redraw reddot
+			drawReddot()
+			//check if exit
+			if (checkExit()){
+				startMaze()
+			}
+		}
+	}
 }
-function drawCircle(ctx, centerX, centerY, radius, reddot = false){
-	ctx.beginPath()
-	ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI)
-	if (reddot) { 
-		ctx.fillStyle = "red" 
-		ctx.fill()
-	} else{
+//check if can move from x,y to x1y1
+function checkWall(xf, yf, xt, yt){
+	if (isDefined(maze)){
+		if (xf-xt > 0){ //move left
+			return maze.field[yf][xf].left
+		}else if (xf-xt < 0) { //move right
+			return maze.field[yf][xf].right
+		}
+		if (yf-yt > 0){ //move up
+			return maze.field[yf][xf].up
+		}else if (yf-yt < 0) { //move down
+			return maze.field[yf][xf].down
+		}
+	}
+}
+//check if reddot in exit
+function checkExit(){
+	if (isDefined(reddot) && isDefined(exit)){
+		return reddot.is(exit)
+	}
+}
+
+//functions to draw stuff
+function getCtx(){
+	let canvas = document.getElementById(`field`)
+	if (canvas.getContext){
+		let ctx = canvas.getContext(`2d`)
+		ctx.restore()
+		return ctx
+	}else {
+		return undefined
+	}
+}
+function clearCanvas(){
+	let ctx = getCtx()
+	if (isDefined(ctx)){
+		let canvas = document.getElementById(`field`)
+		ctx.clearRect(0, 0, canvas.width, canvas.height)
+		ctx.save()
+	}
+}
+function drawExit(){
+	if (isDefined(exit)){
+		let ctx = getCtx()
+		if (isDefined(ctx)){
+			ctx.beginPath()
+			ctx.strokeStyle = "red"
+			ctx.strokeRect(exit.x*sqfd + sqfd/4, exit.y*sqfd + sqfd/4 ,sqfd/2,sqfd/2); 
+		}
+	}
+}
+function drawGradient(){
+	if (isDefined(reddot)){
+		let ctx = getCtx()	
+		ctx.beginPath()
+		let gradient = ctx.createRadialGradient(sqfd*reddot.x + sqfd/2, sqfd*reddot.y + sqfd/2, 50, sqfd*reddot.x + sqfd/2, sqfd*reddot.y + sqfd/2, 100);
+		gradient.addColorStop(0, 'white');
+		gradient.addColorStop(1, 'black');
+		ctx.fillStyle = gradient;
+		ctx.fillRect(0, 0, 640, 480);
+		ctx.save()
+	}
+}
+function drawMaze(){
+	if (isDefined(maze)){
+		let ctx = getCtx()
+		ctx.strokeStyle = "black"
+		if (isDefined(ctx)){
+			var startX = 0;
+			var startY = 0;
+			for (let i = 0; i < maze.height; i++){
+				for (let j = 0; j < maze.width; j++){
+					if (maze.field[i][j].up){
+						drawLine(startX, startY, startX + sqfd, startY)
+					}
+					if (maze.field[i][j].right){
+						drawLine(startX + sqfd, startY, startX + sqfd, startY + sqfd)
+					}
+					if (maze.field[i][j].down){
+						drawLine(startX, startY + sqfd, startX + sqfd, startY + sqfd)
+					}
+					if (maze.field[i][j].left){
+						drawLine(startX, startY, startX, startY + sqfd)
+					}
+					startX += sqfd
+				}
+				startX = 0
+				startY += sqfd
+			}
+			ctx.save()
+		}
+	}
+}
+function drawLine(startX, startY, endX, endY){
+	let ctx = getCtx()
+	if (isDefined(ctx)){
+		ctx.beginPath()
+		ctx.moveTo(startX, startY)
+		ctx.lineTo(endX, endY)
 		ctx.stroke()
+		ctx.save()
 	}
-	
+}
+function drawCircle(centerX, centerY, radius, reddot = false){
+	let ctx = getCtx()
+	if (isDefined(ctx)){
+		ctx.beginPath()
+		ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI)
+		if (reddot) { 
+			ctx.fillStyle = "red" 
+			ctx.fill()
+		} else{
+			ctx.stroke()
+		}
+		ctx.save()
+	}
+}
+function drawReddot(){
+	if (isDefined(reddot)){
+		drawCircle(sqfd*reddot.x + sqfd/2, sqfd*reddot.y + sqfd/2 , (sqfd-5)/2, true)
+		/*
+		var gradient = ctx.createRadialGradient(sqfd*reddot.x + sqfd/2, sqfd*reddot.y + sqfd/2, 50, sqfd*reddot.x + sqfd/2, sqfd*reddot.y + sqfd/2, 100);
+		gradient.addColorStop(0, 'white');
+		gradient.addColorStop(1, 'black');
+		ctx.fillStyle = gradient;
+		ctx.fillRect(0, 0, 640 480);
+		*/
+	}
 }
 
 //classes
+class Point {
+	constructor(x, y){
+		this.x = x
+		this.y = y
+	}
+	is(point){
+		return (this.x == point.x && this.y == point.y)
+	}
+}
 class Sqf {
 	constructor(up = false, right = false, down = false, left = false){
 		this.up = up
@@ -105,7 +306,6 @@ class SqfField {
 				this.field[i][j] = new Sqf()
 			}
 		}
-		//this.filed.map
 	}
 	createMaze(type){
 		//borders are the same
@@ -149,10 +349,12 @@ class SqfField {
 					//place right
 					if (j != this.width-1){
 						if (this.field[i][j].index == this.field[i][j+1].index){
-							this.field[i][j].right = true
+							this.field[i][j].right = true //place right to this box
+							this.field[i][j+1].left = true //and to right for easy check of path
 						}else{
 							if (Math.random() < rPlace) { 
-								this.field[i][j].right = true 
+								this.field[i][j].right = true //place right to this box
+								this.field[i][j+1].left = true //and to right for easy check of path
 							}else {
 								this.field[i][j+1].index = this.field[i][j].index
 							}
@@ -196,12 +398,25 @@ class SqfField {
 				for (let j = 0; j < this.width; j++) {
 					if (line[j].right) {
 						line[j].right = false
+						if (j != this.width){
+							line[j+1].left = false
+						}
+					}
+					if (line[j].left){
+						line[j].left = false
+					}
+					//remove all ups because we must match current line downs to next line ups
+					if (line[j].up){
+						line[j].up = false
 					}
 					if (line[j].down){
 						line[j].index = maxindex++
 						line[j].down = false
+						//match downs and ups
+						line[j].up = true
 					}
 				}
+				//place nextline
 				this.field[i+1] = line
 			}else{
 				//last line
@@ -210,6 +425,7 @@ class SqfField {
 					if (j != this.width-1){
 						if (this.field[i][j].index == this.field[i][j+1].index){
 							this.field[i][j].right = true
+							this.field[i][j+1].left = true
 						}
 					}
 				}
